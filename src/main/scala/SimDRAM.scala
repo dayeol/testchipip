ab48f4c999aae194b2e594db838f12ae20b75414
Howard Mao <zhehao.mao@gmail.com>
2017-02-08 14:00:21 -0800

    fix SimDRAM

--- src/main/scala/SimDRAM.scala
+++ src/main/scala/SimDRAM.scala
@@ -6,16 +6,16 @@ import cde.Parameters
 import uncore.agents.CacheBlockBytes
 
 class SimDRAM(implicit p: Parameters) extends BlackBox {
-  val io = new Bundle {
-    val clock = Clock(INPUT)
-    val reset = Bool(INPUT)
-    val axi = new NastiIO().flip
-  }
+  val io = IO(new Bundle {
+    val clock = Input(Clock())
+    val reset = Input(Bool())
+    val axi = Flipped(new NastiIO())
+  })
 
   val nastiKey = p(NastiKey)
 
   require(nastiKey.idBits == 5)
-  require(nastiKey.addrBits== 32)
-  require(nastiKey.dataBits== 64)
+  require(nastiKey.addrBits == 32)
+  require(nastiKey.dataBits == 64)
   require(p(CacheBlockBytes) == 64)
 }
